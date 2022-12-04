04f12fdb284dfb5c0a27ba54837b6e2195af07ce
r-tavakoli <42064303+r-tavakoli@users.noreply.github.com>
2022-12-04 08:56:10 +0330

    first commit

---
+++ list_tables_of_database.sql
@@ -0,0 +1,15 @@
+--LIST TABLES OF DATABASE
+
+USE test
+GO
+
+SELECT 
+	schema_name(t.schema_id) as schema_name,
+	t.name as table_name,
+	t.create_date,
+	t.modify_date
+FROM
+	sys.tables AS t
+ORDER BY 
+	schema_name,
+	table_name
