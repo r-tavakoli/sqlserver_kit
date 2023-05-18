1c86ec08975a9e3aba911989b1d011124dd5906d
r-tavakoli <42064303+r-tavakoli@users.noreply.github.com>
2023-05-18 10:48:15 +0330

    add file

--- partitioning.sql
+++ partitioning.sql
@@ -221,17 +221,3 @@ INNER JOIN sys.destination_data_spaces dds ON dds.partition_scheme_id = ps.data_
 INNER JOIN sys.filegroups fg ON dds.data_space_id = fg.data_space_id
 LEFT OUTER JOIN sys.partition_range_values rv ON f.function_id = rv.function_id AND p.partition_number = rv.boundary_id
 ORDER BY o.name, fg.name
-
-SELECT fg.data_space_id AS FGID,
-   (f.file_id) AS File_Id,
-   -- As provided by OP, size on disk in bytes.
-   CAST(f.size AS FLOAT) * 8.00 * 1024 AS Size_On_Disk_Bytes,
-   ROUND((CAST(f.size AS FLOAT) * 8.00/1024)/1024,3) AS Actual_File_Size,
-   ROUND(CAST((f.size) AS FLOAT)/128,2) AS Reserved_MB,
-   ROUND(CAST((FILEPROPERTY(f.name,'SpaceUsed')) AS FLOAT)/128,2) AS Used_MB,
-   ROUND((CAST((f.size) AS FLOAT)/128)-(CAST((FILEPROPERTY(f.name,'SpaceUsed'))AS FLOAT)/128),2) AS Free_MB,
-   f.name,
-   f.physical_name
-FROM sys.database_files f 
-    LEFT JOIN sys.filegroups fg
-    ON f.data_space_id = fg.data_space_id
